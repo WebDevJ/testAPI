1d8d76b85d3b7c1363cd96de47d8ba6b941a3bab
WebDevJ <WebDevJ@users.noreply.github.com>
2024-09-10 12:31:01 -0400

    quick api deploy-test (https://testapi-zehs.onrender.com)

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+let obj = {
+    "users": {
+        "id": 0,
+        "Title": "SWE",
+        "popup": {
+            "menuitem": [
+                { "value": "New", "onclick": "CreateNewDoc()" },
+                { "value": "Open", "onclick": "OpenDoc()" },
+                { "value": "Close", "onclick": "CloseDoc()" }
+            ]
+        }
+    }
+}
+
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(
-        {
-            id: 0,
-            user: "James",
-            department: "SWE"
-        },
-        {
-            id: 1,
-            user: "John",
-            department: "SWE"
-        },
+        obj
     ));
 
 })
