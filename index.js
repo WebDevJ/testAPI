46947a0ee3594505c7e188e732053e791c064dd7
WebDevJ <WebDevJ@users.noreply.github.com>
2025-03-03 16:04:53 -0500

    added /nai route

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ let obj = {
     }
 }
 
+let nai = {
+    0: {
+        "National Average Income": "55,000.00",
+        "Title": "NAI",
+        "popup": {
+            "menuitem": [
+                { "value": "New", "onclick": "CreateNewDoc()" },
+                { "value": "Open", "onclick": "OpenDoc()" },
+                { "value": "Close", "onclick": "CloseDoc()" }
+            ]
+        }
+    }
+}
+
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(
@@ -24,7 +38,14 @@ app.get('/', (req, res) => {
 
 })
 
+// National Average Income
+app.get('/nai', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(
+        nai
+    ));
 
+})
 
 
 
